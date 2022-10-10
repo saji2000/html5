7452f93c0ad79b534ca41fe15a3abe5b99af234f
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2022-10-10 15:02:04 -0600

    random number generator in js

--- module.js
+++ module.js
@@ -1,24 +1,15 @@
-var input1 = window.prompt("Enter first number: ", "0");
-var input2 = window.prompt("Enter second number: ", "0");
-var input3 = window.prompt("Enter third number: ", "0");
+var value;
 
-var value1 = parseFloat(input1);
-var value2 = parseFloat(input2);
-var value3 = parseFloat(input3);
+document.writeln("<p>Random numbers</p><ol>");
 
-var values = [value1, value2, value3];
+for (var i = 1; i <= 30; ++i){
 
-var maxValue = maximum(value1, value2, value3);
+    value = Math.floor(1 + Math.random() * 6);
 
-for(var x = 1; x <= values.length; x++){
-    document.writeln("<p>Number #" + x + 
-    " is " + values[x-1] + "</p>");
+    document.writeln("<li>" + value + "</li>");
+
 }
 
-document.writeln("<p>Maximum number is " + maxValue);
-
-function maximum(a, b, c){
-    return Math.max(a, Math.max(b, c));
-}
+document.writeln("</ol>");
 
 
