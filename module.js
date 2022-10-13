b08ca2476c42549bc494007dccc1e94adf711e8b
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2022-10-13 16:00:46 -0600

    arrays in js

--- module.js
+++ module.js
@@ -1,20 +1,33 @@
-var output = "";
+function start(){
+    var n1 = new Array(5);
+    var n2 = new Array();
 
-function calculateFactorials(){
-    for(var i=0; i <= 10; ++i){
-        output += "<p>" + i + "! = " + factorial(i) + "</p>";
+    var length = n1.length;
 
-        document.getElementById("results").innerHTML = output;
+    for (var i = 0; i < length; ++i){
+        n1[i] = i;
     }
+
+    for (var i = 0; i < 10; ++i){
+        n2[i] = i;
+    }
+
+    outputArray("Array n1:", n1, document.getElementById("output1"));
+    outputArray("Array n2:", n2, document.getElementById("output2"));
 }
 
-function factorial(n){
-    if(n <= 1){
-        return 1;
-    }
-    else{
-        return n * factorial(n - 1);
-    }
-}
-window.addEventListener("load", calculateFactorials, false);
+function outputArray(heading, theArray, output){
+    var length = theArray.length;
 
+    var content = "<h2>" + heading + "</h2><table><thead><th>Index</th><th>Value</th></thead><tbody>";
+    
+    for (var i = 0; i < length; i++) {
+        content += "<tr><td>" + i + "</td><td>" + theArray[i] + "</td></tr>";
+    }
+
+    content += "</tbody></table>";
+    output.innerHTML = content;
+
+}
+
+window.addEventListener("load", start, false);
