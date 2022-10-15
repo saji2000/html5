37481c589a34e942dfd30492a232cd5cdf788039
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2022-10-15 15:37:49 -0600

    methods in js, index

--- module.js
+++ module.js
@@ -1,21 +1,18 @@
-var letters = "oandfoganlkabgalkgnqo"
+function split(){
+    var inputString = document.getElementById("inputField").value;
+    var tokens = inputString.split(" ");
 
-function butttonPressed(){
-    var inputField = document.getElementById("inputField");
+    var results = document.getElementById("results");
 
-    document.getElementById("results").innerHTML = 
-        "<p>First occurrenece is located at index " + 
-        letters.indexOf(inputField.value) + "</p>" + 
-        "<p>Last occurrenece is located at index " + 
-        letters.lastIndexOf(inputField.value) + "</p>" +
-        "<p>First occurrenece from index 4 is located at index " + 
-        letters.indexOf(inputField.value, 4) + "</p>" +
-        "<p>Last occurrences from index 4 is located at index " + 
-        letters.lastIndexOf(inputField.value, 4) + "</p>";
+    results.innerHTML = "<p>The sentence split into words is: </p>" + 
+    "<p class = 'indent'>" + 
+    tokens.join("</p><p class = 'indet'>") + "</p>" + 
+    "<p>The first 10 characters of the input string are : </p>" + 
+    "<p class = 'indent'>'" +inputString.substring(0, 10) + "'</p>";
 }
 
 function start(){
-    var searchButton = document.getElementById("searchButton");
-    searchButton.addEventListener("click", butttonPressed, false);
+    var searchButton = document.getElementById("splitButton");
+    searchButton.addEventListener("click", split, false);
 }
 window.addEventListener("load", start, false);
