3d143362164579b1ba58b1c5888d6b97bd0e488b
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2022-10-15 15:06:56 -0600

    methods in js, index

--- module.js
+++ module.js
@@ -1,24 +1,21 @@
-function start(){
-    var s = "ZEBRA";
-    var s2 = "AbCdEfG";
-    var result = "";
+var letters = "oandfoganlkabgalkgnqo"
 
-    result = "<p> Character at index 0 in '" + s + "' is " + 
-        s.charAt(0) + "</p>";
+function butttonPressed(){
+    var inputField = document.getElementById("inputField");
 
-    result += "<p> Character at index 1 in '" + s + "' is " + 
-        s.charCodeAt(0) + "</p>";
-    
-    result += "<p>'" + String.fromCharCode(87, 79, 82, 68) + 
-        "' contains character codes 87, 79, 82, and 68 </p>";
-
-    result += "<p>'" + s2 + "' in lowercase is '" + 
-        s2.toLowerCase() + "'</p>";
-    
-    result += "<p>'" + s2 + "' in uppercase is '" + 
-        s2.toUpperCase() + "'</p>";
-
-    document.getElementById("results").innerHTML = result;
+    document.getElementById("results").innerHTML = 
+        "<p>First occurrenece is located at index " + 
+        letters.indexOf(inputField.value) + "</p>" + 
+        "<p>Last occurrenece is located at index " + 
+        letters.lastIndexOf(inputField.value) + "</p>" +
+        "<p>First occurrenece from index 4 is located at index " + 
+        letters.indexOf(inputField.value, 4) + "</p>" +
+        "<p>Last occurrences from index 4 is located at index " + 
+        letters.lastIndexOf(inputField.value, 4) + "</p>";
 }
 
+function start(){
+    var searchButton = document.getElementById("searchButton");
+    searchButton.addEventListener("click", butttonPressed, false);
+}
 window.addEventListener("load", start, false);
