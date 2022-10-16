dcc5ede6ed138990d7671b8511de67645df58fe4
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2022-10-16 17:45:33 -0600

    methods in js, index

--- module.js
+++ module.js
@@ -1,22 +1,70 @@
-function start(){
-    var current = new Date();
+var tags;
 
-    document.getElementById("strings").innerHTML = 
-        "<p>toString: " + current.toString() + "</p>" +
-        "<p>toLocaleString: " + current.toLocaleString() + "</p>";
+function loadSearches(){
+    if(!sessionStorage.getItem("herePreviously")){
+        sessionStorage.setItem("herePreviously", "true");
+        document.getElementById("welcomeMessage").innerHTML = 
+            "welcome to the favorite twitter searches app";
+    }
 
-    document.getElementById("getMethods").innerHTML = 
-        "<p>getDate: " + current.getDate() + "</p>" +
-        "<p>getDay: " + current.getDay() + "</p>";
+    var length = localStorage.length; 
+    tags = [];
 
-    var anotherDate = new Date(2011, 2, 18, 1, 5, 0 ,0);
-    document.getElementById("newArguments").innerHTML = 
-        "<p>Date: " + anotherDate + "</p>";
+    for(var i=0; i<length; i++){
+        tags[i] = localStorage.key(i);
+    }
 
-    anotherDate.setDate(31);
-    anotherDate.setMonth(11);
+    tags.sort();
 
-    document.getElementById("setMethods").innerHTML = 
-        "<p>Modified date: " + anotherDate + "</p>";
+    var markup = "<ul>"; 
+    var url = "https:search.twitter.com/search?q=";
+
+    for(var tag in tags) {
+
+        var query = url + localStorage.getItem(tags[tag]);
+        markup += "<li><span><a href = '" + query + "'>" + tags[tag] + 
+            "</a></span>" + "<input id = '" + tags[tag] + "' type = 'button' " +
+            "value = 'Edit' onClick = 'editTag(id)'>" + 
+            "<input id = '" + tags[tag] + "' type = 'button' " +
+            "value = 'Delete' onClick = 'deleteTag(id)'>";
+    }
+
+    mrakup += "</ul>";
+    document.getElementById("searches").innerHTML = markup;
+
 }
-window.addEventListener("load", start, false);
+
+function clearAllSearches(){
+    localStorage.clear();
+    loadSearches();
+}
+
+function saveSearch(){
+
+    var query = document.getElementById("query");
+    var tag = document.getElementById("tag");
+
+    localStorage.setItem(tag.value, query.value);
+    tag.value = "";
+    query.value = "";
+    loadSearches();
+}
+
+function deleteTag(tag){
+    localStorage.removeItem(tag);
+    loadSeraches();
+}
+
+function editTag(tag){
+    document.getElementById("query").value = localStorage[tag];
+    document.getElementById("tag").value = tag;
+    loadSearches();
+}
+
+function start(){
+    var saveButton = document.getElementById("saveButton");
+    saveButton.addEventListener("click", saveSearch, false);
+    var clearButton = document.getElementById("clearButton");
+    clearButton.addEventListener("click", clearAllSearches, false);
+    loadSearches();
+}
