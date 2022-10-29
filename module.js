1d142434e5d235f700adeeb9c851572858ef56bd
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2022-10-29 13:53:44 -0600

    DOM tree in HTML5, start and byId functions are done

--- module.js
+++ module.js
@@ -1,70 +1,23 @@
-var tags;
-
-function loadSearches(){
-    if(!sessionStorage.getItem("herePreviously")){
-        sessionStorage.setItem("herePreviously", "true");
-        document.getElementById("welcomeMessage").innerHTML = 
-            "welcome to the favorite twitter searches app";
-    }
-
-    var length = localStorage.length; 
-    tags = [];
-
-    for(var i=0; i<length; i++){
-        tags[i] = localStorage.key(i);
-    }
-
-    tags.sort();
-
-    var markup = "<ul>"; 
-    var url = "https:search.twitter.com/search?q=";
-
-    for(var tag in tags) {
-
-        var query = url + localStorage.getItem(tags[tag]);
-        markup += "<li><span><a href = '" + query + "'>" + tags[tag] + 
-            "</a></span>" + "<input id = '" + tags[tag] + "' type = 'button' " +
-            "value = 'Edit' onClick = 'editTag(id)'>" + 
-            "<input id = '" + tags[tag] + "' type = 'button' " +
-            "value = 'Delete' onClick = 'deleteTag(id)'>";
-    }
-
-    mrakup += "</ul>";
-    document.getElementById("searches").innerHTML = markup;
-
-}
-
-function clearAllSearches(){
-    localStorage.clear();
-    loadSearches();
-}
-
-function saveSearch(){
-
-    var query = document.getElementById("query");
-    var tag = document.getElementById("tag");
-
-    localStorage.setItem(tag.value, query.value);
-    tag.value = "";
-    query.value = "";
-    loadSearches();
-}
-
-function deleteTag(tag){
-    localStorage.removeItem(tag);
-    loadSeraches();
-}
-
-function editTag(tag){
-    document.getElementById("query").value = localStorage[tag];
-    document.getElementById("tag").value = tag;
-    loadSearches();
-}
+var currentNode;
+var idcount = 0;
 
 function start(){
-    var saveButton = document.getElementById("saveButton");
-    saveButton.addEventListener("click", saveSearch, false);
-    var clearButton = document.getElementById("clearButton");
-    clearButton.addEventListener("click", clearAllSearches, false);
-    loadSearches();
+    document.getElementById("byIdButton").addEventListener("click", byId, false);
+    document.getElementById("insertButton").addEventListener("click", insert, false);
+    document.getElementById("appendButton").addEventListener("click", append, false);
+    document.getElementById("replaceButton").addEventListener("click", replace, false);
+    document.getElementById("removeButton").addEventListener("click", remove, false);
+    document.getElementById("parentButton").addEventListener("click", parent, false);
+    currentNode = document.getElementById("bigheading");
+}
+
+window.addEventListener("load", start, false);
+
+function byId(){
+    var id = document.getElementById("gbi").value;
+    var target = document.getElementById(id);
+
+    if(target)
+        switchTo(target);
+
 }
