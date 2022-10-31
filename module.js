9c95317fe36891d73ab841f27322b6d14099a967
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2022-10-31 13:48:19 -0600

    DOM tree in HTML5, all the functions done but not working

--- module.js
+++ module.js
@@ -21,3 +21,60 @@ function byId(){
         switchTo(target);
 
 }
+
+function insert(){
+    var newNode = createNewNode(document.getElementById("ins").value);
+    currentNode.parentNode.insertBefore(newNode, currentNode);
+    switchTo(newNode);
+}
+
+function appendNode(){
+    var newNode = createNewNode(document.getElementById("append").value);
+    currentNode.appendChild(newNode);
+    switchTo(newNode);
+}
+
+function replaceCurrent(){
+    var newNode = createNewNode(document.getElementById("replace").value);
+    currentNode.parentNode.replaceChild(newNode, currentNode);
+    switchTo(newNode);
+}
+
+function remove(){
+    if (currentNode.parentnode == document.body){
+        alert("can't remove a top-level element");
+    }
+    else{
+        var oldNode = currentNode;
+        switchTo(oldNode.parentNode);
+        currentNode.removeChild(oldNode);
+    }
+}
+
+function parent(){
+    target = currentNode.parentNode;
+
+    if(target != document.body)
+        switchTo(target);
+    else
+        alert("no parent element");
+}
+
+
+function createNewNode(){
+    var newNode = document.createElement("p");
+
+    nodeId = "new" + idcount;
+    ++idcount;
+    newNode.setAttribute("id", nodeId);
+    text = "[" + nodeId + "]" + text;
+    newNode.appendChild(document.createTextNode(text));
+    return newNode;
+}
+
+function switchTo(newNode){
+    currentNode.setAttribute("class", ""); 
+    currentNode = newNode;
+    currentNode.setAttribute("class", "highlighted"); 
+    document.getElementById("gbi").value = currentNode.getAttribute("id");
+}
