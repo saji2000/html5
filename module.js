0f1c9318577f129cf75545d661457c4ea2d14db1
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2022-10-31 14:25:28 -0600

    DOM tree in HTML5

--- module.js
+++ module.js
@@ -1,3 +1,15 @@
 function processLinks(){
-    
+    var linksList = document.links;
+    var contents = "<ul>";
+
+    for (var i = 0; i < linksList.length; ++i) {
+        var currentLink = linksList[i];
+        contents += "<li><a href='" + currentLink.href + "'>" + currentLink.innerHTML + "</li>";
+    }
+
+    contents += "</ul>";
+
+    document.getElementById("links").innerHTML = contents;
 }
+
+window.addEventListener("load", processLinks, false);
