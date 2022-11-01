bdfde9821cc65977b2a1a38e049ab849e309aa9a
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2022-11-01 14:23:57 -0600

    event bubbling

--- module.js
+++ module.js
@@ -1,3 +1,21 @@
 function documentClick(){
-    
+    alert("You clicked in the document.");
 }
+
+function bubble(e){
+    alert("This will bubble");
+    e.cancelBubble = false;
+}
+
+function noBubble(){
+    alerty("this will not bubble");
+    e.cancelBubble = true;
+}
+
+function registerEvents(){
+    document.addEventListener("click", documentClick, false);
+    document.getElementById("bubble").addEventListener("click", bubble, false);
+    document.getElementById("noBubble").addEventListener("click", noBubble, false);
+}
+
+window.addEventListener("load", registerEvents, false);
