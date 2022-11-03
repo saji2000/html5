cf0963f7bed84b7b841461ea47c2ee1ef414cfe3
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2022-11-03 14:48:18 -0600

    Blur in Canvas HTML5

--- module.js
+++ module.js
@@ -1,27 +1,12 @@
 function start(){
     var canvas = document.getElementById('drawRectangle');
     var context = canvas.getContext('2d');
-    context.beginPath();
-    context.arc(35, 50, 30, 0, Math.PI * 2);
-    context.fillStyle = "mediumslateblue";
-    context.fill();
-
-    // draw an arc counterclockwise
-    context.beginPath();
-    context.arc(110, 50, 30, 0, Math.PI, false); 
-    context.stroke();
-
-    // draw a half-circle clockwise
-    context.beginPath();
-    context.arc(185, 50, 30, 0, Math.PI, true); 
-    context.fillStyle = "red";
-    context.fill();
-
-    // draw an arc counterclockwise
-    context.beginPath();
-    context.arc(260, 50, 30, 0, 3 * Math.PI / 2);
-    context.strokeStyle = "darkorange";
-    context.stroke();
+    context.shadowBlue = 10;
+    context.shadowOffsetX = 15;
+    context.shadowOffsetY = 15;
+    context.shadowColor = "blue";
+    context.fullStyle = "cyan";
+    context.fillRect(25, 25, 200, 200);
 }
 
 window.addEventListener("load", start, false);
