a531b8e22525403f39d1131ac06cd088ff21e03b
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2022-11-03 14:55:42 -0600

    Quadratic curves in Canvas HTML5

--- module.js
+++ module.js
@@ -7,9 +7,9 @@ function start(){
     context.lineTo(95, 5);
     context.quadraticCurveTo(105, 5, 105, 15);
     context.lineTo(15, 105);
-    context.quadraticCurveTo(105, 105, 5, 95);
+    context.quadraticCurveTo(105, 105, 95, 105);
     context.lineTo(5, 15);
-    context.quadraticCurveTo(5, 5, 15, 5);
+    context.quadraticCurveTo(5, 105, 15, 5);
     context.closePath();
     context.fillStyle = "yellow";
     context.fill();context.strokeStyle = "blue";
