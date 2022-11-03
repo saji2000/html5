ea0a11cd8b7c57e8306ae787837d963db6468937
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2022-11-03 14:45:30 -0600

    Drawing circles in Canvas HTML5

--- module.js
+++ module.js
@@ -3,11 +3,24 @@ function start(){
     var context = canvas.getContext('2d');
     context.beginPath();
     context.arc(35, 50, 30, 0, Math.PI * 2);
-    context.fillStyle = "mediumblue";
+    context.fillStyle = "mediumslateblue";
     context.fill();
 
+    // draw an arc counterclockwise
     context.beginPath();
-    context.arc(110, 50, 30, 0, Math.PI / 2, true);
+    context.arc(110, 50, 30, 0, Math.PI, false); 
+    context.stroke();
+
+    // draw a half-circle clockwise
+    context.beginPath();
+    context.arc(185, 50, 30, 0, Math.PI, true); 
+    context.fillStyle = "red";
+    context.fill();
+
+    // draw an arc counterclockwise
+    context.beginPath();
+    context.arc(260, 50, 30, 0, 3 * Math.PI / 2);
+    context.strokeStyle = "darkorange";
     context.stroke();
 }
 
