9a74ba4d5e029507b5623017ea4c1edba2eb1e86
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2022-11-03 14:51:00 -0600

    Blur in Canvas HTML5

--- module.js
+++ module.js
@@ -1,12 +1,19 @@
 function start(){
     var canvas = document.getElementById('drawRectangle');
     var context = canvas.getContext('2d');
-    context.shadowBlue = 10;
+    context.shadowBlur = 10;
     context.shadowOffsetX = 15;
     context.shadowOffsetY = 15;
     context.shadowColor = "blue";
-    context.fullStyle = "cyan";
+    context.fillStyle = "cyan";
     context.fillRect(25, 25, 200, 200);
+
+    context.shadowBlur = 20;
+    context.shadowOffsetX = -20;
+    context.shadowOffsetY = -20;
+    context.shadowColor = "gray";
+    context.fillStyle = "magenta";
+    context.fillRect(300, 25, 200, 200);
 }
 
 window.addEventListener("load", start, false);
