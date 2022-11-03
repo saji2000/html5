d8d4727db54712680e72569d6eb9d417e30b9227
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2022-11-03 14:44:53 -0600

    Drawing circles in Canvas HTML5

--- module.js
+++ module.js
@@ -5,6 +5,10 @@ function start(){
     context.arc(35, 50, 30, 0, Math.PI * 2);
     context.fillStyle = "mediumblue";
     context.fill();
+
+    context.beginPath();
+    context.arc(110, 50, 30, 0, Math.PI / 2, true);
+    context.stroke();
 }
 
 window.addEventListener("load", start, false);
