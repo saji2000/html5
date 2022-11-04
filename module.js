d280ec91c39bdccd46a91762c044f7d6c5f85a6e
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2022-11-04 13:29:27 -0600

    Skew in Canvas HTML5

--- module.js
+++ module.js
@@ -1,18 +1,23 @@
-var canvas = document.getElementById("drawRectangle");
-var context = canvas.getContext("2d")
 
-function startRotating()
+
+function start()
 {
-context.translate(canvas.width / 2, canvas.height / 2);
-setInterval(rotate, 10);
+    var canvas = document.getElementById("drawRectangle");
+    var context = canvas.getContext("2d");
+    var rectangleWidth = 120;
+    var rectangleHeight = 60;
+    var scaleX = 2;
+    var skewY = 0;
+    var skewX = 1; 
+    var scaleY = 1;
+    var translationX = -10;
+    var translationY = 30;
+    context.translate(canvas.width / 2, canvas.height / 2);
+    context.transform(scaleX, skewY, skewX, scaleY, 
+       translationX, translationY);
+    context.fillStyle = "red";
+    context.fillRect(-rectangleWidth / 2, -rectangleHeight / 2, 
+       rectangleWidth, rectangleHeight);
 }
 
-function rotate() 
-{
-context.clearRect(-100, -100, 200, 200); 
-context.rotate(Math.PI / 360);
-context.fillStyle = "lime";
-context.fillRect(-50, -50, 100, 100);
-}
-
-window.addEventListener( "load", startRotating, false );
+window.addEventListener( "load", start, false );
