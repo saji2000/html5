580db302a039a871ddee1b5d97be464187752456
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2022-11-04 12:53:00 -0600

    Radial Gradients in Canvas HTML5

--- module.js
+++ module.js
@@ -1,7 +1,7 @@
 function start(){
     var canvas = document.getElementById('drawRectangle');
     var context = canvas.getContext('2d');
-    var gradient = context.createLinearGradient(0, 0, 0, 200);
+    var gradient = context.createRadialGradient(100, 100, 10, 100, 100, 125);
 
     gradient.addColorStop(0, "white");
     gradient.addColorStop(0.5, "black");
@@ -11,7 +11,7 @@ function start(){
 
     var canvas = document.getElementById('drawRectangle2');
     var context = canvas.getContext('2d');
-    var gradient = context.createLinearGradient(0, 200, 0, 0);
+    var gradient = context.createRadialGradient(20, 150, 10, 100, 100, 125);
 
     gradient.addColorStop(0, "white");
     gradient.addColorStop(0.5, "black");
