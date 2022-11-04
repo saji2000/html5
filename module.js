854bff29853f8a9ead3bf345509b40d3a1480f20
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2022-11-04 13:55:26 -0600

    Alpha in Canvas HTML5

--- module.js
+++ module.js
@@ -4,8 +4,16 @@ function start()
 {
     var canvas = document.getElementById("drawRectangle");
     var context = canvas.getContext("2d");
-    context.fillStyle = "yellow";
-    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.beginPath();
+    context.rect(10, 10, 120, 120);
+    context.fillStyle = "purple";
+    context.fill();
+    context.globalAlpha = 0.9; 
+    context.beginPath();
+    context.arc(120, 120, 65, 0, 2 * Math.PI, false);
+    context.fillStyle = "lime";
+    context.fill();
+    
 }
 
 window.addEventListener( "load", start, false );
