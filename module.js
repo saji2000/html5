48c74073a953e6f1fc4fd5e1295b553db13998e3
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2022-11-04 12:54:50 -0600

    Images in Canvas HTML5

--- module.js
+++ module.js
@@ -1,23 +1,10 @@
+var image = new Image();
+image.src = "./logo.png";
+
 function start(){
     var canvas = document.getElementById('drawRectangle');
     var context = canvas.getContext('2d');
-    var gradient = context.createRadialGradient(100, 100, 10, 100, 100, 125);
-
-    gradient.addColorStop(0, "white");
-    gradient.addColorStop(0.5, "black");
-    gradient.addColorStop(1, "blue");
-    context.fillStyle = gradient;
-    context.fillRect(0, 0, 200, 200);
-
-    var canvas = document.getElementById('drawRectangle2');
-    var context = canvas.getContext('2d');
-    var gradient = context.createRadialGradient(20, 150, 10, 100, 100, 125);
-
-    gradient.addColorStop(0, "white");
-    gradient.addColorStop(0.5, "black");
-    gradient.addColorStop(1, "blue");
-    context.fillStyle = gradient;
-    context.fillRect(0, 0, 200, 200);
+    context.drawImage(image, 0, 0, 175, 175);
 }
 
 window.addEventListener("load", start, false);
