afd40e4467755fa465960902743577267962e1d4
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2022-11-04 12:51:39 -0600

    Gradients in Canvas HTML5

--- module.js
+++ module.js
@@ -11,7 +11,7 @@ function start(){
 
     var canvas = document.getElementById('drawRectangle2');
     var context = canvas.getContext('2d');
-    var gradient = context.createLinearGradient(0, 0, 0, 200);
+    var gradient = context.createLinearGradient(0, 200, 0, 0);
 
     gradient.addColorStop(0, "white");
     gradient.addColorStop(0.5, "black");
