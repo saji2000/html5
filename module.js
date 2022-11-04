61bb9e20da54f1c561d72bbc8bd2a942581ca639
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2022-11-04 13:32:11 -0600

    Fonts in Canvas HTML5

--- module.js
+++ module.js
@@ -4,20 +4,15 @@ function start()
 {
     var canvas = document.getElementById("drawRectangle");
     var context = canvas.getContext("2d");
-    var rectangleWidth = 120;
-    var rectangleHeight = 60;
-    var scaleX = 2;
-    var skewY = 0;
-    var skewX = 1; 
-    var scaleY = 1;
-    var translationX = -10;
-    var translationY = 30;
-    context.translate(canvas.width / 2, canvas.height / 2);
-    context.transform(scaleX, skewY, skewX, scaleY, 
-       translationX, translationY);
     context.fillStyle = "red";
-    context.fillRect(-rectangleWidth / 2, -rectangleHeight / 2, 
-       rectangleWidth, rectangleHeight);
+    context.font = "italic 24px serif";
+    context.textBaseline = "top";
+    context.fillText("HTML5 Canvas", 0, 0);
+    context.font = "bold 30px sans-serif";
+    context.textAlign = "center";
+    context.lineWidth = 2;
+    context.strokeStyle = "navy";
+    context.strokeText("HTML5 Canvas", 115, 50);
 }
 
 window.addEventListener( "load", start, false );
