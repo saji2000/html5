245fdd6978ca10ee91b43008d90abf660c18fab8
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2022-11-04 13:49:07 -0600

    Resize in Canvas HTML5

--- module.js
+++ module.js
@@ -4,15 +4,8 @@ function start()
 {
     var canvas = document.getElementById("drawRectangle");
     var context = canvas.getContext("2d");
-    context.fillStyle = "red";
-    context.font = "italic 24px serif";
-    context.textBaseline = "top";
-    context.fillText("HTML5 Canvas", 0, 0);
-    context.font = "bold 30px sans-serif";
-    context.textAlign = "center";
-    context.lineWidth = 2;
-    context.strokeStyle = "navy";
-    context.strokeText("HTML5 Canvas", 115, 50);
+    context.fillStyle = "yellow";
+    context.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 window.addEventListener( "load", start, false );
