be8b0a60860a7186e86f15f8b61f5d9d9df1ca4a
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2022-11-14 14:03:50 -0700

    Ajax async listeners, pictures not working since the link is broken

--- module.js
+++ module.js
@@ -1,42 +1,65 @@
 var asyncRequest;
-var ids = ["cpp", "www"];
 
 function registerListeners() {
-    var book;
-    // for (var i = 0; i < ids.size(); ++i){
-    book = document.getElementById(ids[0]);
-    book.addEventListener("mouseover", function(){ switchText(ids[0]);}, false);
-    book.addEventListener("mouseout", clearContent, false);
-
-    book = document.getElementById(ids[1]);
-    book.addEventListener("mouseover", function(){ switchText(ids[1]);}, false);
-    book.addEventListener("mouseout", clearContent, false);
-    // }
-    // console.log()
+    document.getElementById( "all" ).addEventListener(
+        "click", function() { getImages( "all.xml" ); }, false ); 
+    document.getElementById( "simply" ).addEventListener(
+        "click", function() { getImages( "simply.xml" ); }, false ); 
+    document.getElementById( "howto" ).addEventListener(
+        "click", function() { getImages( "howto.xml" ); }, false );
+    document.getElementById("none").addEventListener("click", clearImages, false );
 }
 
-function switchText(name){
-    name = name + ".html";
+function getImages(url){
     try{
         asyncRequest = new XMLHttpRequest();
-
-        asyncRequest.addEventListener("readystatechange", stateChange, false);
-        asyncRequest.open("GET", name, true);
+        asyncRequest.addEventListener( "readystatechange", processImages, false );
+        asyncRequest.open("GET", url, true );
         asyncRequest.send(null);
     }
     catch(e){
-        alert("request error");
+        alert("error: " + e.message);
     }
 }
 
-function stateChange(){
-    if(asyncRequest.readyState == 4 && asyncRequest.status == 200){
-        document.getElementById("contentArea").innerHTML = asyncRequest.responseText;
+function processImages(){
+    if(asyncRequest.readyState == 4 && asyncRequest.status == 200 && asyncRequest.responseXML){
+        clearImages();
+
+        var covers = asyncRequest.responseXML.getElementByTagName("covers");
+        var baseUrl = asyncRequest.responseXML.getElementsByTagName( 
+            "baseurl" ).item( 0 ).firstChild.nodeValue;
+
+        var output = document.getElementById( "covers" );
+        
+        // create an unordered list to display the images
+        var imagesUL = document.createElement( "ul" );
+        
+        // place images in unordered list
+        for ( var i = 0; i < covers.length; ++i )
+        {
+            var cover = covers.item( i ); // get a cover from covers array
+        
+            // get the image filename
+            var image = cover.getElementsByTagName( "image" ).
+                item( 0 ).firstChild.nodeValue;
+        
+            // create li and img element to display the image
+            var imageLI = document.createElement( "li" );
+            var imageTag = document.createElement( "img" );
+            
+            // set img element's src attribute
+            imageTag.setAttribute( "src", baseUrl + escape( image ) ); 
+            imageLI.appendChild( imageTag ); // place img in li
+            imagesUL.appendChild( imageLI ); // place li in ul
+        } // end for statement
+
+        output.appendChild( imagesUL );
     }
 }
 
-function clearContent(){
-    document.getElementById("contentArea").innerHTML = "";
+function clearImages(){
+    document.getElementById("covers").innerHTML = "";
 }
 
 window.addEventListener("load", registerListeners, false);
