21707d06721d3cd08e616c75ef1f75b784c206af
Seyed Sajjad Daneshmand <sajaddaneshmand79@gmail.com>
2022-11-13 20:36:45 -0700

    Ajax async

--- module.js
+++ module.js
@@ -1,59 +1,39 @@
-var doc; // variable to reference the XML document
-var outputHTML = ""; // stores text to output in outputDiv
+var asyncRequest;
+var ids = ["cpp", "www"];
 
-// register event handler for button and load XML document
-function start()
-{
-   document.getElementById( "matchesButton" ).addEventListener( 
-      "click", processXPathExpression, false );
-   loadXMLDocument( "practice.xml" );
-} // end function start
 
-// load XML document programmatically
-function loadXMLDocument( url )
-{
-   var xmlHttpRequest = new XMLHttpRequest();
-   xmlHttpRequest.open( "get", url, false );
-   xmlHttpRequest.send( null );
-   doc = xmlHttpRequest.responseXML;
-} // end function loadXMLDocument
+function registerListeners() {
+    var book;
+    for (var i = 0; i < ids.length; ++i){
+        book = document.getElementById(ids[i]);
+        book.addEventListener("mouseover", switchText(ids[i]), false);
+        book.addEventListener("mouseout", clearContent, false);
+    }
+    
+}
 
-// display the XML document 
-function displayHTML()
-{
-   document.getElementById( "outputDiv" ).innerHTML = outputHTML;
-} // end function displayDoc
+function switchText(name){
+    name = name + ".html";
+    try{
+        asyncRequest = new XMLHttpRequest();
 
-// obtain and apply XPath expression
-function processXPathExpression()
-{
-   var xpathExpression = document.getElementById( "inputField" ).value;
-   var result;
-   outputHTML = "";
+        asyncRequest.addEventListener("readystatechange", stateChange, false);
+        asyncRequest.open("GET", url, true);
+        asyncRequest.send(null);
+    }
+    catch(e){
+        alert("request error");
+    }
+}
 
-   if ( !doc.evaluate ) // Internet Explorer 
-   {
-      result = doc.selectNodes( xpathExpression );
+function stateChange(){
+    if(asyncRequest.readyState == 4 && asyncRequest.status == 200){
+        document.getElementById("contentArea").innerHTML = asyncRequest.responseText;
+    }
+}
 
-      for ( var i = 0; i < result.length; i++ )
-      {
-         outputHTML += "<p>" + result.item( i ).text + "</p>";
-      } // end for
-   } // end if
-   else // other browsers
-   {
-      result = doc.evaluate( xpathExpression, doc, null, 
-        XPathResult.ORDERED_NODE_ITERATOR_TYPE, null );
-      var current = result.iterateNext();
+function clearContent(){
+    document.getElementById("contentArea").innerHTML = "";
+}
 
-      while ( current ) 
-      {
-         outputHTML += "<p>" + current.textContent + "</p>";
-         current = result.iterateNext();
-      } // end while
-   } // end else
-
-   displayHTML();
-} // end function processXPathExpression
-
-window.addEventListener( "load", start, false );
+window.addEventListener("load", registerListeners, false);
